d6dfcc907d9ff9b08ea966b65ff0214b8a1676ef
Jonathan Páez <jonathan.paez@bayteq.com>
2022-05-17 16:37:14 -0500

    JPA - SnakesAndLadders:  Corrección pruebas unitarias textos

--- SnakesAndLadders/SnakesAndLaddersTest/UnitTest1.cs
+++ SnakesAndLadders/SnakesAndLaddersTest/UnitTest1.cs
@@ -24,82 +24,82 @@ namespace SnakesAndLaddersTest
         public void TokenMove3SpacesUS1UAT2()
         {
             BoardBE board = boardLogic.CreateBoard(playerLogic.CreatePlayers(2));
-            var playerTurn = board.Players.Find(player => player.HasTurn);
+            var currentPlayer = boardLogic.GetCurrentPlayer(board.Players);
             // Given the token is on square 1
-            Assert.AreEqual(playerTurn.Position, 1);
+            Assert.AreEqual(currentPlayer.Position, 1);
             //When the token is moved 3 spaces
-            playerTurn.Position = boardLogic.MovePlayer(
+            currentPlayer.Position = boardLogic.MovePlayer(
                 3,
                 board,
-                playerTurn
+                currentPlayer
             );
             // Then the token is on square 4
-            Assert.AreEqual(playerTurn.Position, 4);
+            Assert.AreEqual(currentPlayer.Position, 4);
         }
 
         [TestMethod]
         public void TokenThrow2DiceUS1UAT3()
         {
             BoardBE board = boardLogic.CreateBoard(playerLogic.CreatePlayers(2));
-            var playerTurn = board.Players.Find(player => player.HasTurn);
+            var currentPlayer = boardLogic.GetCurrentPlayer(board.Players);
             // Given the token is on square 1
-            Assert.AreEqual(playerTurn.Position, 1);
+            Assert.AreEqual(currentPlayer.Position, 1);
             //When the token is moved 3 spaces
-            playerTurn.Position = boardLogic.MovePlayer(
+            currentPlayer.Position = boardLogic.MovePlayer(
                 3,
                 board,
-                playerTurn);
+                currentPlayer);
             // Then the token is on square 4
-            Assert.AreEqual(playerTurn.Position, 4);
+            Assert.AreEqual(currentPlayer.Position, 4);
             // And then it is moved 4 spaces
-            playerTurn.Position = boardLogic.MovePlayer(
+            currentPlayer.Position = boardLogic.MovePlayer(
                 4,
                 board,
-                playerTurn);
+                currentPlayer);
             // Then the token is on square 8
-            Assert.AreEqual(playerTurn.Position, 8);
+            Assert.AreEqual(currentPlayer.Position, 8);
         }
         [TestMethod]
         public void WinGameUS2UAT1()
         {
             BoardBE board = boardLogic.CreateBoard(playerLogic.CreatePlayers(2));
-            var playerTurn = board.Players.Find(player => player.HasTurn);
-            playerTurn.Position = boardLogic.MovePlayer(
+            var currentPlayer = boardLogic.GetCurrentPlayer(board.Players);
+            currentPlayer.Position = boardLogic.MovePlayer(
                 96,
                 board,
-                playerTurn);
+                currentPlayer);
             // Given the token is on square 97
-            Assert.AreEqual(playerTurn.Position, 97);
+            Assert.AreEqual(currentPlayer.Position, 97);
             // When the token is moved 3 spaces
-            playerTurn.Position = boardLogic.MovePlayer(
+            currentPlayer.Position = boardLogic.MovePlayer(
                 3,
                 board,
-                playerTurn);
+                currentPlayer);
             // Then the token is on square 100
-            Assert.AreEqual(playerTurn.Position, 100);
+            Assert.AreEqual(currentPlayer.Position, 100);
             // And the player has won the game
-            Assert.AreEqual(boardLogic.IsPlayerWinner(board, playerTurn), true);
+            Assert.AreEqual(boardLogic.IsPlayerWinner(board, currentPlayer), true);
         }
         [TestMethod]
         public void NotWinGameUS2UAT2()
         {
             BoardBE board = boardLogic.CreateBoard(playerLogic.CreatePlayers(2));
-            var playerTurn = board.Players.Find(player => player.HasTurn);
-            playerTurn.Position = boardLogic.MovePlayer(
+            var currentPlayer = boardLogic.GetCurrentPlayer(board.Players);
+            currentPlayer.Position = boardLogic.MovePlayer(
                 96,
                 board,
-                playerTurn);
+                currentPlayer);
             // Given the token is on square 97
-            Assert.AreEqual(playerTurn.Position, 97);
+            Assert.AreEqual(currentPlayer.Position, 97);
             // When the token is moved 4 spaces
-            playerTurn.Position = boardLogic.MovePlayer(
+            currentPlayer.Position = boardLogic.MovePlayer(
                 4,
                 board,
-                playerTurn);
-            // Then the token is on square 100
-            Assert.AreEqual(playerTurn.Position, 97);
-            // And the player has won the game
-            Assert.AreEqual(boardLogic.IsPlayerWinner(board, playerTurn), false);
+                currentPlayer);
+            // Then the token is on square 97
+            Assert.AreEqual(currentPlayer.Position, 97);
+            // And the player has not won the game
+            Assert.AreEqual(boardLogic.IsPlayerWinner(board, currentPlayer), false);
         }
         [TestMethod]
         public void RollDiceUS3UAT1()
@@ -115,16 +115,16 @@ namespace SnakesAndLaddersTest
         public void Move4ApaceUS3UAT2()
         {
             BoardBE board = boardLogic.CreateBoard(playerLogic.CreatePlayers(2));
-            var playerTurn = board.Players.Find(player => player.HasTurn);
-            var initialTokenPosition = playerTurn.Position;
+            var currentPlayer = boardLogic.GetCurrentPlayer(board.Players);
+            var initialTokenPosition = currentPlayer.Position;
             // Given the player rolls a 4
-            playerTurn.Position = boardLogic.MovePlayer(
+            currentPlayer.Position = boardLogic.MovePlayer(
                 4,
                 board,
-                playerTurn);
+                currentPlayer);
             // When they move their token
             // Then the token should move 4 spaces
-            Assert.AreEqual(playerTurn.Position, initialTokenPosition + 4);
+            Assert.AreEqual(currentPlayer.Position, initialTokenPosition + 4);
         }
     }
 }
